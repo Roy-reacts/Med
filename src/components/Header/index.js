a51f26b3c3729978420aa9147f1d3b5e672f2e38
Roy-reacts <83531710+Roy-reacts@users.noreply.github.com>
2021-07-14 16:51:07 +0600

    Update

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.css';
+
+ const Header = () => {
+    return (
+     <header className = "header">
+     <nav className =" headerMenu">
+     <a href = "/"> Our story</a>
+     <a href = "/"> Membership</a>
+
+     <a href = "/"> Write </a>
+     <a href = "/"> Sign In</a>
+
+     </nav>
+     
+     </header>
+    )
+}
+ export default Header;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './style.css';
+
+const Navbar = (props) => {
+    return (
+        <div className ="navbar">
+        <ul className= "navbarMenu">
+        <li> <a href ="/"> Home</a></li>
+        <li> <a href ="/"> Posts</a></li>
+        <li> <a href ="/"> About us</a></li>
+        <li> <a href ="/"> Contact us</a></li>
+
+        
+        </ul>
+        <div className ="search">
+        <input type= "text" placeholder ="Search"/>
+        <img src ={require('../../assets/icons/search.png')}alt="Search"/>
+        </div>
+            
+        </div>
+    )
+}
+
+export default Navbar
